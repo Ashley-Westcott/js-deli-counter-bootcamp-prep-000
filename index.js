db12456f3d31b9acd2349a38e35be875c352f3e3
algebraic-torrent-6062 <ashley.e.westcott@gmail.com>
2019-03-11 06:39:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -14,8 +14,7 @@ function nowServing (katzDeliLine){
   if (katzDeliLine.length === 0){
     return `There is nobody waiting to be served!`
   } else {
-    katzDeliLine.shift()
-    return katzDeliLine
+    return `Currently serving` + katzDeliLine()
   }
 }
 
